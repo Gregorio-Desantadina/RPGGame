f5eadd6c55704c321126737fd01bd13226891ed8
Gregorio <gregoriodesantadina@gmail.com>
2023-10-24 11:00:24 -0300

    Cambio de prueba

--- Game/Main.cs
+++ Game/Main.cs
@@ -13,10 +13,9 @@ namespace MyGameProject.Game.Start
 
             Character Character1 = new  IceMage(input);
 
-            Character Character2 = new ArcherEsqueleton("Esqueleton1");
+            Character Character2 = new ArcherEsqueleton("Esqueleton");
 
             Character Character3 = new Executioner ("Amigo");
-
 
             Character Character5 = new Ghost("Ghost");
 
